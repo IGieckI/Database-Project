075a03cddedd10642ab426a4bf2b0de5fd2b71dc
IGieckI <giacomo.antonelli3@gmail.com>
2023-05-30 00:40:16 +0200

    SQL file update: Create db and tables

---
+++ Database Project/Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Database_Project
+{
+    internal class Database
+    {
+        string connectionString;
+        SqlConnection cnn;
+        //connectionString = @"Data Source=PC1227;Initial Catalog=Magazzino;User ID=sa;Password=burbero2020";
+        connectionString = $@"Data Source=DESKTOP-CDHTOA2;Initial Catalog = ASPAdventure; Integrated Security=SSPI;";
+                cnn = new SqlConnection(connectionString);
+        SqlDataReader OutPutSelectAll;
+    }
+}
