5f88ce021d4beb45538b95172acb9580d6e12119
IGieckI <giacomo.antonelli3@gmail.com>
2023-06-21 20:35:42 +0200

    Generated a BankAccount record for Bank related information

---
+++ Database Project/Entities/BankAccount.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_Project.Entities
+{
+    public record BankAccount(int BankAccountID, string IBAN, string BankName, string BIC_SWIFT);
+}
