ad0a1098eb1a6102c4b645b7ebddf870e81a3dd7
IGieckI <giacomo.antonelli3@gmail.com>
2023-06-23 12:03:30 +0200

    Account changes: add credit and change telephone type

--- Database Project/Entities/Account.cs
+++ Database Project/Entities/Account.cs
@@ -6,6 +6,6 @@ using System.Threading.Tasks;
 
 namespace Database_Project.Entities
 {
-    public record Account(string Username, string Email, string Password, string Street, int CivicAddress, int Cap, 
-        string City, string Country, int TelephoneNumber);
+    public record Account(string Username, string Email, string Password, string? Street, int? CivicNumber, int? Cap, 
+        string? City, string? Country, string TelephoneNumber, int Credit);
 }
