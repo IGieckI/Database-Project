75221e5081822e0d6ddc4c2b3a4fb10f7e035c9e
IGieckI <giacomo.antonelli3@gmail.com>
2023-06-23 12:04:26 +0200

    Implemented User profile page (still missing fields update stuff)

--- Database Project/Pages/Profile.xaml.cs
+++ Database Project/Pages/Profile.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Database_Project.Entities;
+using Database_Project.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,29 @@ namespace Database_Project.Pages
     /// </summary>
     public partial class Profile : Page
     {
+        private List<WishlistItem> _wishlist = new List<WishlistItem>();
+        private Account _account;
+        private readonly IDatabase _database = new DatabaseImpl();
+
         public Profile()
         {
             InitializeComponent();
+
+            _account = _database.GetUserAccount(MainWindow.Username);
+
+            lblUsername.Content = _account.Username;
+            lblCredit.Content = _account.Credit;
+            txtEmail.Text = _account.Email;
+            txtStreet.Text = _account.Street;
+            txtCivicNumber.Text = _account.CivicNumber.ToString();
+            txtCap.Text = _account.Cap.ToString();
+            txtCity.Text = _account.City;
+            txtCountry.Text = _account.Country;
+            txtTelephoneNumber.Text = _account.TelephoneNumber.ToString();
+
+            _wishlist = _database.GetWishlist(MainWindow.Username);
+            grdWishlist.ItemsSource = _wishlist;
+            grdWishlist.Items.Refresh();
         }
     }
 }
