f9e5fced562166192c81e7615be8ea2438e2a040
IGieckI <giacomo.antonelli3@gmail.com>
2023-06-23 12:03:49 +0200

    Add WishlistItem record

---
+++ Database Project/Entities/WishlistItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_Project.Entities
+{
+    public record WishlistItem(string Name, int Quantity, string Rarity, string Game, string Expansion);
+}
