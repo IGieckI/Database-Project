7af97e40bf4e5753e0f19ff3b205e08ffa354f3b
IGieckI <giacomo.antonelli3@gmail.com>
2023-06-23 09:04:40 +0200

    Add a record representing an Offert

---
+++ Database Project/Entities/Offert.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_Project.Entities
+{
+    public record Offert(int Id, float price, int quantity, string conditions, string language, string location, int ProductId);
+}
