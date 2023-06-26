205ce0bda16fa10adb5888fcb2b2a2a80f775ac0
IGieckI <giacomo.antonelli3@gmail.com>
2023-06-26 14:57:26 +0200

    Created a mask for shopping cart elements

---
+++ Database Project/Entities/ShoppingCartElement.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_Project.Entities
+{
+    public record ShoppingCartElement(string Name, decimal Price, int Quantity, string Seller);
+}
